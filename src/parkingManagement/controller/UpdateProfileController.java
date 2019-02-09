5ab7d457ec6b44aeae1d91e36064b878f413f129
tshakthiprakash <tshakthiprakash@gmail.com>
2019-02-09 16:24:30 -0600

    UpdateController_new changes

--- src/parkingManagement/controller/UpdateProfileController.java
+++ src/parkingManagement/controller/UpdateProfileController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import parkingManagement.model.*;
 import parkingManagement.data.*;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/UpdateProfileController")
 public class UpdateProfileController extends HttpServlet {
@@ -16,17 +17,30 @@ public class UpdateProfileController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		User user = new User(request.getParameter("firstname"), request.getParameter("lastname"),
-				request.getParameter("username"), request.getParameter("password"), request.getParameter("uta_id"),
+				request.getParameter("username"), request.getParameter("password"),request.getParameter("password"), request.getParameter("uta_id"),
 				request.getParameter("user_role"), request.getParameter("phone"), request.getParameter("email"),
 				request.getParameter("saddress"), request.getParameter("city"), request.getParameter("state"),
 				request.getParameter("zip"), request.getParameter("car_num_plate"), request.getParameter("permit_id"),
 				request.getParameter("permit_type"));
-		user.setFirstname(request.getParameter("firstname"));
-		user.setUsername(request.getParameter("username"));
-		ProfileDao profileDao = new ProfileDao();
-		profileDao.updateUser(user);
-		response.sendRedirect("ProfileController");
+		
+		RegisterUserErrorMsgs ProfileErrorMsgs = new RegisterUserErrorMsgs();
+		user.validateUser(user, ProfileErrorMsgs,"myprofile");
+		session.setAttribute("user",user);
+		session.setAttribute("profileerrorMsgs",ProfileErrorMsgs);
+		if (ProfileErrorMsgs.getErrorMsg().equals("")){
+			
+			ProfileDao profileDao = new ProfileDao();
+			profileDao.updateUser(user);
+			response.sendRedirect("ProfileController");
+		}
+		else
+		{
+			response.sendRedirect("ProfileController");	
+		}
+		
+		
 
 	}
 
