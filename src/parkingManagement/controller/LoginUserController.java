325fc699646c274d37a23c5004b32da8774813e3
tshakthiprakash <tshakthiprakash@gmail.com>
2019-02-10 09:09:38 -0600

    Update LoginUserController.java

--- src/parkingManagement/controller/LoginUserController.java
+++ src/parkingManagement/controller/LoginUserController.java
@@ -1,32 +1,67 @@
 package parkingManagement.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import parkingManagement.data.*;
+import parkingManagement.model.User;
+import parkingManagement.data.LoginUserDao;
 import parkingManagement.model.*;
 import javax.servlet.http.HttpSession;
-@WebServlet("/ProfileController")
-public class ProfileController extends HttpServlet {
+
+@WebServlet("/loginUserController")
+public class LoginUserController extends HttpServlet {
+	String url="";
+	String errMsg="";
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LoginErrorMsg loginErr = new LoginErrorMsg();
+		LoginUserDao regDb = new LoginUserDao();
 		HttpSession session = request.getSession();
-		ProfileDao profileDao = new ProfileDao();
-		User user;
-		User temp = (User)session.getAttribute("user_info");
-		user = profileDao.fetchUserProfile(temp.getUsername());
-		//session.setAttribute("my_profile", user);
-		int count = (int)session.getAttribute("myprofileCount");
-		//System.out.println(count);
-		if(count==0){
-		session.removeAttribute("myprofileCount");
-		session.setAttribute("myprofileCount", 1);
-		session.setAttribute("my_profile", user);
+		session.removeAttribute("errorMessage");
+		session.removeAttribute("incorrectpass");
+		User login_user = new User();
+		User dbuser = new User();
+		if((request.getParameter("login_username").equals("")) || (request.getParameter("login_password").equals("")))
+		{
+			url ="/index.jsp";
+			errMsg = "Please enter the Username or Password";
+			loginErr.setUserNameErrMsg(errMsg);
+			session.setAttribute("errorMessage", loginErr);
+			getServletContext().getRequestDispatcher(url).forward(request, response);
 		}
-		response.sendRedirect("myprofile.jsp");
+	else
+		{
+			session.setAttribute("user_info", login_user);
+			login_user.setUsername(request.getParameter("login_username"));
+			login_user.setPassword(request.getParameter("login_password"));
+			dbuser = regDb.searchUser(request.getParameter("login_username"));
+			if(dbuser.getPassword().equals(login_user.getPassword()))
+				{
+				session.setAttribute("myprofileCount", 0);
+				if(dbuser.getRole().equals("Student/Faculty")){
+				response.sendRedirect("student_faculty.jsp");session.setAttribute("home", "student_faculty.jsp");
+				}
+				else if(dbuser.getRole().equals("Manager")){
+				response.sendRedirect("manager.jsp");session.setAttribute("home", "manager.jsp");}
+				else if(dbuser.getRole().equals("Admin")){
+				response.sendRedirect("admin.jsp");session.setAttribute("home", "admin.jsp");}
+				
+				}
+			else
+				{	
+					url ="/index.jsp";
+					errMsg="Incorrect Username or Password";
+					loginErr.setUserNameErrMsg(errMsg);
+					session.setAttribute("incorrectpass", loginErr);
+					getServletContext().getRequestDispatcher(url).forward(request, response);
+				}
+	
+		}
 	}
 }
