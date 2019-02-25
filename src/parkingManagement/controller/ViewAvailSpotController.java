c71a1362d442208bd75fda226dac3e5fb3c2d990
tshakthiprakash <tshakthiprakash@gmail.com>
2019-02-25 01:57:39 -0600

    changes releated to the makespot unavailable and revoking it

--- src/parkingManagement/controller/ViewAvailSpotController.java
+++ src/parkingManagement/controller/ViewAvailSpotController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.*;
+import parkingManagement.model.UnavailableSpot;
 
 import parkingManagement.data.ParkingspotDao;
 
@@ -16,25 +17,60 @@ public class ViewAvailSpotController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		String action=request.getParameter("action");
 		ParkingspotDao parkDao = new ParkingspotDao();
+		HttpSession session = request.getSession();
+		if(action.equals("numberavailable")){
 		ArrayList<String> parkingareanames = new ArrayList<String>();
 		parkingareanames = parkDao.getparkingareaname();
-		HttpSession session = request.getSession();
+		
 		session.removeAttribute("avail_spots");
+		session.removeAttribute("unavailable_list");
 		session.setAttribute("parkingAreaNames", parkingareanames);
 		response.sendRedirect("view_avail_spot.jsp");
 		}
+		}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		String action = request.getParameter("action");
+		ParkingspotDao parkDao = new ParkingspotDao();
+		HttpSession session= request.getSession();
+		
+		if(action.equals("noavailspots")){
 		String parkingareaname = request.getParameter("parkingareaname");
 		String fromTime = request.getParameter("fromtime");
 		String toTime = request.getParameter("totime");
 		String permit_type=request.getParameter("permit_type");
-		ParkingspotDao parkDao = new ParkingspotDao();
 		int avilable_spots=parkDao.number_avail_spot(parkingareaname, fromTime, toTime,permit_type);
-		HttpSession session= request.getSession();
 		session.setAttribute("avail_spots",avilable_spots );
 		response.sendRedirect("view_avail_spot.jsp");
+		}
+		
+		if(action.equals("makeunavailable"))
+		{
+			UnavailableSpot unavailspot = new UnavailableSpot();
+			unavailspot.setParkingName(request.getParameter("parkingareaname"));
+			unavailspot.setType(request.getParameter("type"));
+			unavailspot.setSpot_no(Integer.parseInt(request.getParameter("spotno")));
+			parkDao.setParkspotunavail(unavailspot);
+			response.sendRedirect("viewAvailSpotController?action=numberavailable");
+			
+		}
+		if(action.equals("listavailable"))
+		{
+		ArrayList<UnavailableSpot> unlist = new ArrayList<UnavailableSpot>();
+		unlist = parkDao.fetch_unavail_spots();
+		session.setAttribute("unavailable_list", unlist);
+		response.sendRedirect("view_avail_spot.jsp");
+			}
+		if(action.equals("removeunavail"))
+		{
+		UnavailableSpot unavail = new UnavailableSpot();
+		unavail.setParkingName(request.getParameter("parking_name"));
+		unavail.setType(request.getParameter("parking_type"));
+		unavail.setSpot_no(Integer.parseInt(request.getParameter("spot_num")));
+		parkDao.remove_unavailable(unavail);
+		response.sendRedirect("viewAvailSpotController?action=numberavailable");
+		}
 	}
 
 }
